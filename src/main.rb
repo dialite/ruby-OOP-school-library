8e82ceff5355d72b88b7489daac95a75194ef456
dialite <raymondolisa01@gmail.com>
2023-02-20 00:15:36 +0100

    Fix rubocop offenses

--- src/main.rb
+++ src/main.rb
@@ -3,8 +3,8 @@ require_relative './decorator/capitalize_decorator'
 require_relative './decorator/trimmer_decorator'
 
 person = Person.new(22, 'maximilianus')
-  person.correct_name
-  capitalizedPerson = CapitalizeDecorator.new(person)
-  capitalizedPerson.correct_name
-  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-  capitalizedTrimmedPerson.correct_name
+person.correct_name
+capitalizedperson = CapitalizeDecorator.new(person)
+capitalizedperson.correct_name
+capitalizedtrimmedperson = TrimmerDecorator.new(capitalizedperson)
+capitalizedtrimmedperson.correct_name
