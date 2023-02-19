ba135db1254d87396e40c189e888b0d0a33b4927
dialite <raymondolisa01@gmail.com>
2023-02-19 22:47:58 +0100

    Correct rubocop offenses

--- src/student.rb
+++ src/student.rb
@@ -3,7 +3,7 @@ require_relative './person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, name = 'Unknown', parent_permission = true, classroom: 'unknown')
+  def initialize(age, name = 'Unknown', parent_permission: true, classroom: 'unknown')
     super(age, name, parent_permission)
     @classroom = classroom
   end
