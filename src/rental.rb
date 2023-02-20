34131de2138664da8872341ae4e88b9c5709eb78
dialite <raymondolisa01@gmail.com>
2023-02-20 10:36:24 +0100

    Rental belongs to a person

--- src/rental.rb
+++ src/rental.rb
@@ -1,11 +1,15 @@
 class Rental
-  attr_accessor :date, :book
+  attr_accessor :date, :book, :person
 
-  def initialize(date, book)
+  def initialize(date, book, person)
     @date = date
 
     # a rental belongs to a book
     @book = book
     book.rentals.push(self)
+
+    # a rental belongs to a person
+    @person = person
+    person.rentals << self
   end  
 end
