9c2652c6c4e62571152b932ff6e9f047e24b6a68
dialite <raymondolisa01@gmail.com>
2023-02-20 17:30:33 +0100

    Add create book method

--- src/app.rb
+++ src/app.rb
@@ -74,6 +74,18 @@ class App
     puts "Teacher with id #{teacher.id} successfully created"
   end
 
+  def create_book
+    puts 'What is the title of the book: '
+    title = gets.chomp
+    puts 'What is the author of the book: '
+    author = gets.chomp
+    book = Book.new(title, author)
+    @books << book
+    puts 'Book created successfully'
+    sleep 0.75
+    @menu.option_list
+  end
+
   def exit
     puts 'Goodbye...'
   end
