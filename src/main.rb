9fbdbb495d0840327f5624c375d06bba211fa133
dialite <raymondolisa01@gmail.com>
2023-02-20 12:28:38 +0100

    Modify menu start in main.rb

--- src/main.rb
+++ src/main.rb
@@ -1,16 +1,17 @@
 class MenuStart
   def initialize
-    options_list
+    option_list
   end
-  def options_list
+
+  def option_list
     puts 'Welcome to School Library!'
-    puts "Kindly choose an option by entering a number: 
+    puts "Kindly choose an option by entering a number:
     1 - List all books
     2 - List all people
     3 - Create a person
     4 - Create a book
     5 - Create a rental
-    6 - List all rentals for a given person id
+    6 - List all rentals for a given person ID
     7 - Exit"
   end
 end
