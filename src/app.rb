788c1411878aab49d075d5d4144c1746f69aa2f4
dialite <raymondolisa01@gmail.com>
2023-02-20 21:08:10 +0100

    Modify app.rb

--- src/app.rb
+++ src/app.rb
@@ -88,16 +88,16 @@ class App
 
   def create_rental
     puts 'Select a book from the following list by number'
-    @books.each_with_index { |book, index| puts "#{index + 1}) Title: #{book.title}, Author: #{book.author}" }
+    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
     book_id = gets.chomp.to_i
-    puts 'Invalid input!' if !(book_id.is_a? Integer) && book_id >= @books.length
+    puts 'Invalid input!' if !(book_id.is_a? Integer) && book_id > @books.length
 
     puts 'Select a person from the following list by number (not id)'
     @people.each_with_index do |person, index|
-      puts "#{index + 1}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
     person_id = gets.chomp.to_i
-    puts 'Invalide input!' if !(person_id.is_a? Integer) && person_id >= @people.length
+    puts 'Invalide input!' if !(person_id.is_a? Integer) && person_id > @people.length
 
     print 'Please enter the date in this format [yyyy-mm-dd]: '
     date = gets.chomp.to_s
@@ -112,8 +112,7 @@ class App
 
   def list_rentals_by_person_id
     puts 'No person entry found' if @people.empty?
-    @menu.option_list
-    print 'Enter person ID: '
+    puts 'Enter person ID: '
     id = gets.chomp.to_i
     @people.each do |person|
       next unless person.id == id
--- src/rental.rb
+++ src/rental.rb
@@ -3,16 +3,10 @@ class Rental
 
   def initialize(date, book, person)
     @date = date
-    add_book(book)
-    add_person(person)
-  end
-
-  def add_book(book)
+    
     @book = book
-    book.rentals << self
-  end
+    book.rentals.push(self)
 
-  def add_person(person)
     @person = person
     person.rentals << self
   end
