fd3d35fe47aa831556109c1a6c21905c9a84a69a
dialite <raymondolisa01@gmail.com>
2023-02-20 09:37:46 +0100

    Create the has-many side of Book and Rental (a book has many rentals)

--- src/book.rb
+++ src/book.rb
@@ -1,7 +1,16 @@
+require_relative './rental'
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
+
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  # A book has many rentals
+  def add_rental(rental)
+    @rentals.push(rental) unless @rentals.include?(rental)
+    rental.book = self
   end
 end
