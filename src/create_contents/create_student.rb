0ef766442d96870dc00ef15257d08b732f13d8f6
dialite <raymondolisa01@gmail.com>
2023-02-21 07:03:07 +0100

    Craete validation for create student

--- src/create_contents/create_student.rb
+++ src/create_contents/create_student.rb
@@ -5,6 +5,12 @@ def add_student
   age = gets.chomp.to_i
   print 'Has parent permission? [Y/N]: '
   parent_permission = gets.chomp.to_s.downcase == 'y'
+
+  name_check = !name.empty? && name.is_a?(String)
+  age_check = age < 18 && age.is_a?(Integer)
+  parent_permission_check = parent_permission.is_a?(String)
+  return puts 'Age should be less than 18' unless name_check && age_check && parent_permission_check
+
   student = Student.new(age, name, parent_permission: parent_permission)
   @people << student
 
