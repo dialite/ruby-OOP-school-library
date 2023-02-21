6710dd01a2813af47a4386e1aafc1632437c0787
dialite <raymondolisa01@gmail.com>
2023-02-21 05:26:09 +0100

    Refactor add teacher and student in relation to add person

--- src/app.rb
+++ src/app.rb
@@ -78,33 +78,6 @@ class App
     @menu.option_list
   end
 
-  def add_student
-    print 'Enter the name of the student: '
-    name = gets.chomp
-    print 'Enter the age of the student: '
-    age = gets.chomp.to_i
-    print 'Has parent permission? [Y/N]: '
-    parent_permission = gets.chomp.to_s.downcase == 'y'
-    student = Student.new(age, name, parent_permission: parent_permission)
-    @people << student
-
-    puts "Student with id #{student.id} successfully created"
-  end
-
-  def add_teacher
-    print 'Enter the name of the teacher: '
-    name = gets.chomp
-    print 'Enter the age of the teacher: '
-    age = gets.chomp.to_i
-    print "And teacher's specialization: "
-    specilization = gets.chomp.to_s
-    teacher = Teacher.new(specilization, age, name)
-    @people << teacher
-
-    puts "Teacher with id #{teacher.id} successfully created"
-  end
-
-
   def exit
     puts 'Goodbye...'
   end
--- src/create_contents/create_person.rb
+++ src/create_contents/create_person.rb
@@ -1,3 +1,6 @@
+require_relative './create_teacher'
+require_relative './create_student'
+
 def create_person
     puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     type = gets.chomp.to_i
