150cec9ab695f5a2ebc47bad22b8a3c7c67a0d83
dialite <raymondolisa01@gmail.com>
2023-02-21 07:09:48 +0100

    Validate create teacher

--- src/create_contents/create_student.rb
+++ src/create_contents/create_student.rb
@@ -1,6 +1,6 @@
 def add_student
   print 'Enter the name of the student: '
-  name = gets.chomp
+  name = gets.chomp.to_s
   print 'Enter the age of the student: '
   age = gets.chomp.to_i
   print 'Has parent permission? [Y/N]: '
@@ -9,7 +9,7 @@ def add_student
   name_check = !name.empty? && name.is_a?(String)
   age_check = age < 18 && age.is_a?(Integer)
   parent_permission_check = parent_permission.is_a?(String)
-  return puts 'Age should be less than 18' unless name_check && age_check && parent_permission_check
+  return puts 'Fill all options and age should be less than 18' unless name_check && age_check && parent_permission_check
 
   student = Student.new(age, name, parent_permission: parent_permission)
   @people << student
--- src/create_contents/create_teacher.rb
+++ src/create_contents/create_teacher.rb
@@ -1,11 +1,17 @@
 def add_teacher
   print 'Enter the name of the teacher: '
-  name = gets.chomp
+  name = gets.chomp.to_s
   print 'Enter the age of the teacher: '
   age = gets.chomp.to_i
   print "And teacher's specialization: "
-  specilization = gets.chomp.to_s
-  teacher = Teacher.new(specilization, age, name)
+  specialization = gets.chomp.to_s
+
+  name_check = !name.empty? && name.is_a?(String)
+  age_check = age >= 18 && age.is_a?(Integer)
+  specialization_check = !specialization.empty? && specialization.is_a?(String)
+  return puts 'Fill all options and age should be equal or more than 18' unless name_check && age_check && specialization_check
+
+  teacher = Teacher.new(specialization, age, name)
   @people << teacher
 
   puts "Teacher with id #{teacher.id} successfully created"
